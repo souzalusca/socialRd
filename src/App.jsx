488b114b08a1701157b7eb02bf7ffaabc643784c
souzalusca <124852772+souzalusca@users.noreply.github.com>
2024-05-17 00:43:22 -0300

    Deixando dinamico com props

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,42 @@ import './global.css'
 import { Sidebar } from './components/Sidebar'
 import { Post } from './components/Post'
 
+//
+
+//author : {avatar_url: "", name: "", role: ""}
+//pubishedAt: Date
+//content: string
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/gustavoguanabara.png',
+      name: 'Gustavo Guanabara',
+      role: 'Educator @ Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Opa, boa noite!' },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. deem uma moral la! ' },
+      { type: 'link', content: 'jane.design/doctorcare' },
+    ],
+    publishedAt: new Date('2024-05-11 08:13:30'),
+  },
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://avatars.githubusercontent.com/u/12782014?v=4',
+      name: 'Hudson Medeiros',
+      role: 'Educator @ Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋' },
+      { type: 'paragraph', content: 'É um projeto que fiz com Lucas, no evento da Rocketseat. O nome do projeto é Rede Social' },
+      { type: 'link', content: 'www.github.com/souzalusca' },
+    ],
+    publishedAt: new Date('2022-05-10 07:13:30'),
+  }
+]
 export function App() {
 
   return (
@@ -15,11 +51,15 @@ export function App() {
     <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post />
-
-          <Post />
-
-          <Post />
+         {posts.map(post => {
+           return ( 
+            <Post
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            /> 
+          )
+         })}
         </main>
 
     </div>
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,29 +1,44 @@
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
-export function Post() {
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
+export function Post({ author, publishedAt, content }) {
+    const publishedDateFormatted = format(publishedAt, "d 'de' LLLL 'às' HH:mm'h'", {
+        locale: ptBR
+
+    })
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        locale: ptBR,
+        addSuffix: true
+    })
+
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                   <Avatar src="https://media.licdn.com/dms/image/C5603AQGmsnnXI5CUAw/profile-displayphoto-shrink_800_800/0/1601434547666?e=1721260800&v=beta&t=HN7y6q7ONqrs7YDnEqd4qRP7G38SlgBe5d_DlrLtuAo" alt="" />
+                   <Avatar src={author.avatarUrl} alt="" />
                     <div className={styles.authorInfo}>
-                        <strong>Marcelo Vilela</strong>
-                        <span>Developer Junior</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
-                <time title='11 de maio às 08:13' dateTime="2022-05-11 08:13:30">Publicado há 1h</time>
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+                    {publishedDateRelativeToNow}
+                </time>
             </header>
 
             <div className={styles.content}>
-                <p>Fala galeraa 👋</p>
-                <p>Meu Amigo é muito pika, deem uma moral pra ele, tmj  🚀</p>
-                <p><a href="#">jane.design/doctorcare</a></p>
-                <p>
-                    <a href="#">#novoprojeto</a>{' '}
-                    <a href="#">#nlw</a>{' '}
-                    <a href="#">#rocketseat</a></p>
+                {content.map(line => {
+                    if (line.type === 'paragraph') {
+                        return <p key={line.content}>{line.content}</p>
+                    } else if (line.type === 'link') {
+                        return <p key={line.content}><a href="">{line.content}</a></p>
+                    }
+                })}
             </div>
 
             <form className={styles.commentForm}>
